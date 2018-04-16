9812b72677ac378fc8e00b43d3a1ff12d6886815
SimonCMills <smills2@sheffield.ac.uk>
2018-04-16 17:49:03 +0100

    initial commit

---
+++ scripts/checkCoords_CzechRepublic.R
@@ -0,0 +1,29 @@
+## checking Czech coordinates. How different are 'site' coords and 'municipality' coords. 
+require(dplyr); require(ggplot2); require(sp)
+coords <- xlsx::read.xlsx("recievedFiles/PECBMS_originalFiles/coords/sites_Czech.xls", 1)
+
+# remove NA values as no comparison to be made
+coords <- coords %>% 
+    filter(!is.na(first_point_LON))
+
+
+
+municipality <- coords %>% 
+    select(long = municipality_LON,
+           lat = municipality_LAT)
+municipality <- SpatialPoints(municipality,  proj4string=CRS("+proj=longlat +datum=WGS84"))
+
+site <- coords %>%
+    select(long = first_point_LON, lat = first_point_LAT)
+site <- SpatialPoints(site,  proj4string=CRS("+proj=longlat +datum=WGS84"))
+
+# dists constructs a pairwise comparison between all points, returning a matrix
+dists<- spDists(municipality, site, longlat = TRUE)
+# diagonals of these are the distances between a municipality point and a site point
+
+ggplot() + geom_histogram(aes(x=diag(dists)), fill="white", col="black", binwidth=.25) + theme_classic() +
+    labs(title="Distances between 'municipality' and 'site' coordinates, where both exist",
+         x="Distance (km)") +
+    theme(plot.title = element_text(hjust=1))
+
+ggsave("figures/CzechRepublic_municipalitySiteDiffs.png", dpi=200)
